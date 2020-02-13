46107dd7bf51dffc847a594adb97546e5bded733
TheHitMan7 <krtik.vrma@gmail.com>
2020-02-13 19:45:31 +0530

    willow: Add sm8150 Display HAL build properties
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -74,6 +74,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_scaler=0 \
     vendor.display.enable_default_color_mode=1 \
     vendor.display.enable_null_display=0 \
+    vendor.display.enable_optimize_refresh=1 \
+    vendor.display.disable_ui_3d_tonemap=1 \
     vendor.gralloc.disable_ubwc=0
 
 # This matrix should be in column major order, per SurfaceFlinger requirement
