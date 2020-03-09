a45f9f4b1db4e0cb596670837ab9cda180ee6b59
weivincewang <wvw@google.com>
2020-03-09 14:54:16 +0530

    willow: Switch to libperfmgr for powerhint
    Signed-off-by: weivincewang <wvw@google.com>
    Signed-off-by: AbhiShek Aggarwal <warabhishek@gmail.com>
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

--- device.mk
+++ device.mk
@@ -341,11 +341,11 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.willow
+    android.hardware.power@1.3-service.willow \
+    powerhint.json
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
